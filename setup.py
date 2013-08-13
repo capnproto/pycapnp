6d3c0117b4c9d201c9a0e27ea4428d63302a9063
Jason Paryani <github@jparyani.com>
2013-08-12 17:48:20 -0700

    Add classifiers to setup.py
    [skip ci]

--- setup.py
+++ setup.py
@@ -65,9 +65,11 @@ setup(
         'Programming Language :: C++',
         'Programming Language :: Cython',
         'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
-        # 'Programming Language :: Python :: Implementation :: PyPy'
+        'Programming Language :: Python :: Implementation :: PyPy'
         'Topic :: Communications'],
 )
