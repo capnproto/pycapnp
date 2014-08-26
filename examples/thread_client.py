95e706f2f94ddfb944fe1a894ced67064b36b248
Jason Paryani <github@jparyani.com>
2014-08-26 15:21:46 -0700

    Add thread examples

---
+++ examples/thread_client.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+
+import argparse
+import threading
+import time
+import capnp
+
+import thread_capnp
+
+capnp.remove_event_loop()
+capnp.create_event_loop(threaded=True)
+
+
+def parse_args():
+  parser = argparse.ArgumentParser(usage='Connects to the Example thread server \
+at the given address and does some RPCs')
+  parser.add_argument("host", help="HOST:PORT")
+
+  return parser.parse_args()
+
+
+class StatusSubscriber(thread_capnp.Example.StatusSubscriber.Server):
+
+    '''An implementation of the StatusSubscriber interface'''
+
+    def status(self, value, **kwargs):
+        print('status: {}'.format(time.time()))
+
+
+def start_status_thread(host):
+  client = capnp.TwoPartyClient(host)
+  cap = client.ez_restore('example').cast_as(thread_capnp.Example)
+
+  subscriber = StatusSubscriber()
+  promise = cap.subscribeStatus(subscriber)
+  promise.wait()
+
+
+def main(host):
+  client = capnp.TwoPartyClient(host)
+  cap = client.ez_restore('example').cast_as(thread_capnp.Example)
+
+  status_thread = threading.Thread(target=start_status_thread, args=(host,))
+  status_thread.daemon = True
+  status_thread.start()
+
+  print('main: {}'.format(time.time()))
+  cap.longRunning().wait()
+  print('main: {}'.format(time.time()))
+  cap.longRunning().wait()
+  print('main: {}'.format(time.time()))
+  cap.longRunning().wait()
+  print('main: {}'.format(time.time()))
+
+if __name__ == '__main__':
+    main(parse_args().host)
