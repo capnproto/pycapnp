53d21f787bb46b79e50e247465c68e7ba211526f
Jason Paryani <github@jparyani.com>
2014-12-12 12:20:13 -0800

    Add O2 and DNDEBUG flags to libcapnp build

--- buildutils/build.py
+++ buildutils/build.py
@@ -14,7 +14,7 @@ def build_libcapnp(bundle_dir, build_dir, verbose=False):
     if verbose:
       stdout = None
     cxxflags = os.environ.get('CXXFLAGS', '')
-    os.environ['CXXFLAGS'] = cxxflags + ' -fPIC'
+    os.environ['CXXFLAGS'] = cxxflags + ' -fPIC -O2 -DNDEBUG'
     conf = subprocess.Popen(['./configure', '--disable-shared', '--prefix', build_dir], cwd=capnp_dir, stdout=stdout)
     returncode = conf.wait()
     if returncode != 0:
