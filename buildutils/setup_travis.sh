5df670ce652b5cbf373b6595298520cd2c6d9d45
Afonso Bordado <afonsobordado@az8.co>
2018-09-15 12:37:42 +0100

    Use correct gcc package

--- buildutils/setup_travis.sh
+++ buildutils/setup_travis.sh
@@ -6,9 +6,9 @@ CAPNP_VERSION=0.5.2
 
 sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
 sudo apt-get -qq update
-sudo apt-get -qq install g++-5.0 libstdc++-5.0-dev
-sudo update-alternatives --quiet --install /usr/bin/gcc  gcc  /usr/bin/gcc-5.0  60 --slave   /usr/bin/g++  g++  /usr/bin/g++-5.0 --slave   /usr/bin/gcov gcov /usr/bin/gcov-5.0
-sudo update-alternatives --quiet --set gcc /usr/bin/gcc-5.0
+sudo apt-get -qq install g++-5 libstdc++-5-dev
+sudo update-alternatives --quiet --install /usr/bin/gcc  gcc  /usr/bin/gcc-5  60 --slave   /usr/bin/g++  g++  /usr/bin/g++-5 --slave   /usr/bin/gcov gcov /usr/bin/gcov-5
+sudo update-alternatives --quiet --set gcc /usr/bin/gcc-5
 
 if ! [ -z "${BUILD_CAPNP}" ]; then
   wget https://capnproto.org/capnproto-c++-${CAPNP_VERSION}.tar.gz && tar xzvf capnproto-c++-${CAPNP_VERSION}.tar.gz && cd capnproto-c++-${CAPNP_VERSION} && ./configure && make -j6 && sudo make install && sudo ldconfig && cd ..
