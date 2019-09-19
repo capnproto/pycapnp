db4e567c6bbbc8b66b12655dbe5206a3a709391e
Jacob Alexander <haata@kiibohd.com>
2019-09-18 19:01:46 -0700

    Upgrading to capnproto-0.7.0
    - Needed for asyncio support

--- buildutils/bundle.py
+++ buildutils/bundle.py
@@ -35,7 +35,7 @@ pjoin = os.path.join
 # Constants
 #-----------------------------------------------------------------------------
 
-bundled_version = (0,6,1)
+bundled_version = (0,7,0)
 libcapnp = "capnproto-c++-%i.%i.%i.tar.gz" % (bundled_version)
 libcapnp_url = "https://capnproto.org/" + libcapnp
 
