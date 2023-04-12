f667ed2a99775e4c6491cbb84fb39d421f584568
Lasse Blaauwbroek <lasse@blaauwbroek.eu>
2023-04-12 06:04:34 +0200

    Bring asyncProvider.cpp more in line with upstream async-io-unix.c++
    It was originally copied from the nodejs implementation, which in turn copied
    from async-io-unix.c++. But that copy is pretty old.

--- capnp/helpers/asyncProvider.cpp
+++ capnp/helpers/asyncProvider.cpp
@@ -1,9 +1,35 @@
-// Adapted from https://github.com/capnproto/node-capnp/blob/node10/src/node-capnp/capnp.cc
-// Original code licensed under BSD 2-clause
+// Adapted from upstream capnproto implementation in async-io-unix.c++.
+// Additionally adapted from the node-capnp implementation:
+//  https://github.com/capnproto/node-capnp/blob/node10/src/node-capnp/capnp.cc
+//
+// Original copyright:
 
+// Copyright (c) 2013-2014 Sandstorm Development Group, Inc. and contributors
+// Licensed under the MIT License:
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+#include <netinet/tcp.h>
 #include <kj/debug.h>
 #include <fcntl.h>
 #include <arpa/inet.h>
+#include <kj/io.h>
 #include <kj/async.h>
 #include <kj/async-io.h>
 #include <kj/vector.h>
@@ -19,20 +45,29 @@
 
 using namespace kj;
 
-static void setNonblocking(int fd) {
+void setNonblocking(int fd) {
+#ifdef FIONBIO
+  int opt = 1;
+  KJ_SYSCALL(ioctl(fd, FIONBIO, &opt));
+#else
   int flags;
   KJ_SYSCALL(flags = fcntl(fd, F_GETFL));
   if ((flags & O_NONBLOCK) == 0) {
     KJ_SYSCALL(fcntl(fd, F_SETFL, flags | O_NONBLOCK));
   }
+#endif
 }
 
-static void setCloseOnExec(int fd) {
+void setCloseOnExec(int fd) {
+#ifdef FIOCLEX
+  KJ_SYSCALL(ioctl(fd, FIOCLEX));
+#else
   int flags;
   KJ_SYSCALL(flags = fcntl(fd, F_GETFD));
   if ((flags & FD_CLOEXEC) == 0) {
     KJ_SYSCALL(fcntl(fd, F_SETFD, flags | FD_CLOEXEC));
   }
+#endif
 }
 
 static int applyFlags(int fd, uint flags) {
@@ -173,43 +208,46 @@ public:
     : OwnedFileDescriptor(fd, flags, fdListener) {}
   virtual ~PyIoStream() noexcept(false) {}
 
-  kj::Promise<size_t> read(void* buffer, size_t minBytes, size_t maxBytes) override {
-    return tryReadInternal(buffer, minBytes, maxBytes, 0).then([=](size_t result) {
-      KJ_REQUIRE(result >= minBytes, "Premature EOF") {
-        // Pretend we read zeros from the input.
-        memset(reinterpret_cast<kj::byte*>(buffer) + result, 0, minBytes - result);
-        return minBytes;
-      }
-      return result;
-    });
-  }
-
   kj::Promise<size_t> tryRead(void* buffer, size_t minBytes, size_t maxBytes) override {
     return tryReadInternal(buffer, minBytes, maxBytes, 0);
   }
 
-  kj::Promise<void> write(const void* buffer, size_t size) override {
-    ssize_t writeResult;
-    KJ_NONBLOCKING_SYSCALL(writeResult = ::write(fd, buffer, size)) {
+  Promise<void> write(const void* buffer, size_t size) override {
+    ssize_t n;
+    KJ_NONBLOCKING_SYSCALL(n = ::write(fd, buffer, size)) {
+      // Error.
+
+      // We can't "return kj::READY_NOW;" inside this block because it causes a memory leak due to
+      // a bug that exists in both Clang and GCC:
+      //   http://gcc.gnu.org/bugzilla/show_bug.cgi?id=33799
+      //   http://llvm.org/bugs/show_bug.cgi?id=12286
+      goto error;
+    }
+    if (false) {
+    error:
       return kj::READY_NOW;
     }
 
-    // A negative result means EAGAIN, which we can treat the same as having written zero bytes.
-    size_t n = writeResult < 0 ? 0 : writeResult;
-
-    if (n == size) {
-      return kj::READY_NOW;
+    if (n < 0) {
+      // EAGAIN -- need to wait for writability and try again.
+      return onWritable().then([=]() {
+        return write(buffer, size);
+      });
+    } else if (n == size) {
+      // All done.
+      return READY_NOW;
     } else {
-      buffer = reinterpret_cast<const kj::byte*>(buffer) + n;
+      // Fewer than `size` bytes were written, but we CANNOT assume we're out of buffer space, as
+      // Linux is known to return partial reads/writes when interrupted by a signal -- yes, even
+      // for non-blocking operations. So, we'll need to write() again now, even though it will
+      // almost certainly fail with EAGAIN. See comments in the read path for more info.
+      buffer = reinterpret_cast<const byte*>(buffer) + n;
       size -= n;
-    }
-
-    return onWritable().then([=]() {
       return write(buffer, size);
-    });
+    }
   }
 
-  kj::Promise<void> write(kj::ArrayPtr<const kj::ArrayPtr<const kj::byte>> pieces) override {
+  Promise<void> write(ArrayPtr<const ArrayPtr<const byte>> pieces) override {
     if (pieces.size() == 0) {
       return writeInternal(nullptr, nullptr);
     } else {
@@ -217,16 +255,27 @@ public:
     }
   }
 
+  // TODO: Potentially copy over more api's from capnproto async-io-unix.c++ like
+  //       writeWithFds, tryPumpFrom and friends to make certain operations faster
+
   void shutdownWrite() override {
     // There's no legitimate way to get an AsyncStreamFd that isn't a socket through the
     // UnixAsyncIoProvider interface.
     KJ_SYSCALL(shutdown(fd, SHUT_WR));
   }
 
+  void abortRead() override {
+    // There's no legitimate way to get an AsyncStreamFd that isn't a socket through the
+    // UnixAsyncIoProvider interface.
+    KJ_SYSCALL(shutdown(fd, SHUT_RD));
+  }
+
   kj::Promise<void> whenWriteDisconnected() override {
-    // TODO(someday): Implement using UV_DISCONNECT?
+    // TODO(someday): Implement using some Python API
     return kj::NEVER_DONE;
   }
+
+  // TODO: Potentially copy over capnpro apis like getsockopt, get peername and getFd
 
 private:
   kj::Promise<size_t> tryReadInternal(void* buffer, size_t minBytes, size_t maxBytes,
@@ -237,6 +286,17 @@ private:
 
     ssize_t n;
     KJ_NONBLOCKING_SYSCALL(n = ::read(fd, buffer, maxBytes)) {
+      // Error.
+
+      // We can't "return kj::READY_NOW;" inside this block because it causes a memory leak due to
+      // a bug that exists in both Clang and GCC:
+      //   http://gcc.gnu.org/bugzilla/show_bug.cgi?id=33799
+      //   http://llvm.org/bugs/show_bug.cgi?id=12286
+      goto error;
+    }
+
+    if (false) {
+    error:
       return alreadyRead;
     }
 
@@ -248,29 +308,36 @@ private:
     } else if (n == 0) {
       // EOF -OR- maxBytes == 0.
       return alreadyRead;
-    } else if (kj::implicitCast<size_t>(n) < minBytes) {
-      // The kernel returned fewer bytes than we asked for (and fewer than we need).  This indicates
-      // that we're out of data.  It could also mean we're at EOF.  We could check for EOF by doing
-      // another read just to see if it returns zero, but that would mean making a redundant syscall
-      // every time we receive a message on a long-lived connection.  So, instead, we optimistically
-      // asume we are not at EOF and return to the event loop.
-      //
-      // If libuv provided notification of HUP or RDHUP, we could do better here...
-      buffer = reinterpret_cast<kj::byte*>(buffer) + n;
+    } else if (implicitCast<size_t>(n) >= minBytes) {
+      // We read enough to stop here.
+      alreadyRead += n;
+      return alreadyRead;
+    } else {
+      // The kernel returned fewer bytes than we asked for (and fewer than we need).
+
+      buffer = reinterpret_cast<byte*>(buffer) + n;
       minBytes -= n;
       maxBytes -= n;
       alreadyRead += n;
-      return onReadable().then([=]() {
-        return tryReadInternal(buffer, minBytes, maxBytes, alreadyRead);
-      });
-    } else {
-      // We read enough to stop here.
-      return alreadyRead + n;
+
+      // According to David Klempner, who works on Stubby at Google, we sadly CANNOT assume that
+      // we've consumed the whole read buffer here. If a signal is delivered in the middle of a
+      // read() -- yes, even a non-blocking read -- it can cause the kernel to return a partial
+      // result, with data still in the buffer.
+      //     https://bugzilla.kernel.org/show_bug.cgi?id=199131
+      //     https://twitter.com/CaptainSegfault/status/1112622245531144194
+      //
+      // Unfortunately, we have no choice but to issue more read()s until it either tells us EOF
+      // or EAGAIN. We used to have an optimization here using observer.atEndHint() (when it is
+      // non-null) to avoid a redundant call to read(). Alas...
+      return tryReadInternal(buffer, minBytes, maxBytes, alreadyRead);
     }
   }
 
   kj::Promise<void> writeInternal(kj::ArrayPtr<const kj::byte> firstPiece,
                                   kj::ArrayPtr<const kj::ArrayPtr<const kj::byte>> morePieces) {
+    // TODO: Update this function to the upstream code in async-io-unix.c++ once this issue is resolved:
+    //       https://github.com/capnproto/capnproto/issues/1658
     KJ_STACK_ARRAY(struct iovec, iov, 1 + morePieces.size(), 16, 128);
 
     // writev() interface is not const-correct.  :(
@@ -331,15 +398,37 @@ public:
   kj::Promise<kj::Own<kj::AsyncIoStream>> accept() override {
     int newFd;
 
+    struct sockaddr_storage addr;
+    socklen_t addrlen = sizeof(addr);
+
   retry:
-#if __linux__
-    newFd = ::accept4(fd, nullptr, nullptr, SOCK_NONBLOCK | SOCK_CLOEXEC);
+#if __linux__ && !__BIONIC__
+    newFd = ::accept4(fd, reinterpret_cast<struct sockaddr*>(&addr), &addrlen,
+                      SOCK_NONBLOCK | SOCK_CLOEXEC);
 #else
-    newFd = ::accept(fd, nullptr, nullptr);
+    newFd = ::accept(fd, reinterpret_cast<struct sockaddr*>(&addr), &addrlen);
 #endif
 
     if (newFd >= 0) {
-      return kj::Own<kj::AsyncIoStream>(kj::heap<PyIoStream>(newFd, NEW_FD_FLAGS, fdListener));
+      kj::AutoCloseFd ownFd(newFd);
+      // TODO(perf):  As a hack for the 0.4 release we are always setting
+      //   TCP_NODELAY because Nagle's algorithm pretty much kills Cap'n Proto's
+      //   RPC protocol.  Later, we should extend the interface to provide more
+      //   control over this.  Perhaps write() should have a flag which
+      //   specifies whether to pass MSG_MORE.
+      int one = 1;
+      KJ_SYSCALL_HANDLE_ERRORS(::setsockopt(
+            ownFd.get(), IPPROTO_TCP, TCP_NODELAY, (char*)&one, sizeof(one))) {
+        case EOPNOTSUPP:
+        case ENOPROTOOPT: // (returned for AF_UNIX in cygwin)
+#if __FreeBSD__
+        case EINVAL: // (returned for AF_UNIX in FreeBSD)
+#endif
+          break;
+        default:
+          KJ_FAIL_SYSCALL("setsocketopt(IPPROTO_TCP, TCP_NODELAY)", error);
+      }
+      return kj::Own<kj::AsyncIoStream>(kj::heap<PyIoStream>(ownFd.release(), NEW_FD_FLAGS, fdListener));
     } else {
       int error = errno;
 
@@ -355,7 +444,10 @@ public:
 
         case EINTR:
         case ENETDOWN:
+#ifdef EPROTO
+        // EPROTO is not defined on OpenBSD.
         case EPROTO:
+#endif
         case EHOSTDOWN:
         case EHOSTUNREACH:
         case ENETUNREACH:
@@ -406,6 +498,10 @@ kj::Own<kj::AsyncIoStream> PyLowLevelAsyncIoProvider::wrapSocketFd(int fd, uint 
 }
 kj::Promise<kj::Own<kj::AsyncIoStream>> PyLowLevelAsyncIoProvider::wrapConnectingSocketFd(
       int fd, const struct sockaddr* addr, uint addrlen, uint flags) {
+  // It's important that we construct the AsyncStreamFd first, so that `flags` are honored,
+  // especially setting nonblocking mode and taking ownership.
+  auto result = kj::heap<PyIoStream>(fd, flags, fdListener);
+
   // Unfortunately connect() doesn't fit the mold of KJ_NONBLOCKING_SYSCALL, since it indicates
   // non-blocking using EINPROGRESS.
   for (;;) {
@@ -424,7 +520,6 @@ kj::Promise<kj::Own<kj::AsyncIoStream>> PyLowLevelAsyncIoProvider::wrapConnectin
     }
   }
 
-  auto result = kj::heap<PyIoStream>(fd, flags, fdListener);
   auto connected = result->onWritable();
   return connected.then(kj::mvCapture(result,
       [fd](kj::Own<kj::AsyncIoStream>&& stream) {
