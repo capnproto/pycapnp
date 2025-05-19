ed8c4adf794cf4aeeb81282c7223217a00980eaa
Brian Xu <brian.xu1@bytedance.com>
2025-05-19 11:52:05 +1000

    refine example

--- examples/py_custom_message_builder.py
+++ examples/py_custom_message_builder.py
@@ -10,8 +10,9 @@ class Allocator:
         self.last_size = 0
 
     def __call__(self, minimum_size: int) -> bytearray:
-        print(f"minimum_size: {minimum_size}, last_size: {self.last_size}, cur_size: {self.cur_size}")
         actual_size = max(minimum_size, self.cur_size)
+        print(f"minimum_size: {minimum_size}, last_size: {self.last_size}, "
+              f"actual_size: {actual_size}, cur_size: {self.cur_size}")
         self.last_size = actual_size
         self.cur_size += actual_size
 
@@ -24,23 +25,23 @@ struct_builder = msg_builder.init_root(addressbook_capnp.Person)
 print()
 
 struct_builder.init("extraData", 5)
-print(struct_builder._get('extraData'))
-print(bytes(struct_builder._get('extraData')))
+print(struct_builder.extraData)
+print(bytes(struct_builder.extraData))
+print(type(struct_builder.extraData))
 print()
 
-struct_builder._get('extraData')[1] = 0xff
-# msg._get('extraData')[100] = 0xff
-print(struct_builder._get('extraData'))
-print(bytes(struct_builder._get('extraData')))
-print(type(struct_builder._get('extraData')))
+struct_builder.extraData[1] = 0xff
+print(struct_builder.extraData)
+print(bytes(struct_builder.extraData))
 print()
 
 struct_builder.extraData = b'hello'
-print(struct_builder._get('extraData'))
-print(bytes(struct_builder._get('extraData')))
-print(type(struct_builder._get('extraData')))
+print(struct_builder.extraData)
+print(bytes(struct_builder.extraData))
+print(type(struct_builder.extraData))
 print()
 
 struct_builder = struct_builder.as_reader()
 print(struct_builder.extraData)
+print(bytes(struct_builder.extraData))
 print(type(struct_builder.extraData))
