78fa3a162ce7a8824c9405902a6d7474d2d19a68
Brian Xu <brian.xu1@bytedance.com>
2025-09-09 17:45:09 +1000

    reformat flake8

--- examples/py_custom_message_builder.py
+++ examples/py_custom_message_builder.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 
 import capnp  # noqa: F401
-
 import addressbook_capnp
+
 
 class Allocator:
     def __init__(self):
@@ -11,14 +11,17 @@ class Allocator:
 
     def __call__(self, minimum_size: int) -> bytearray:
         actual_size = max(minimum_size, self.cur_size)
-        print(f"minimum_size: {minimum_size}, last_size: {self.last_size}, "
-              f"actual_size: {actual_size}, cur_size: {self.cur_size}")
+        print(
+            f"minimum_size: {minimum_size}, last_size: {self.last_size}, "
+            f"actual_size: {actual_size}, cur_size: {self.cur_size}"
+        )
         self.last_size = actual_size
         self.cur_size += actual_size
 
         WORD_SIZE = 8
         byte_count = actual_size * WORD_SIZE
         return bytearray(byte_count)
+
 
 person = addressbook_capnp.Person.new_message(allocate_seg_callable=Allocator())
 
@@ -28,12 +31,12 @@ print(bytes(person.extraData))
 print(type(person.extraData))
 print()
 
-person.extraData[1] = 0xff
+person.extraData[1] = 0xFF
 print(person.extraData)
 print(bytes(person.extraData))
 print()
 
-person.extraData = b'hello'
+person.extraData = b"hello"
 print(person.extraData)
 print(bytes(person.extraData))
 print(type(person.extraData))
