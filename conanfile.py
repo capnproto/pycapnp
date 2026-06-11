451ad52a678d7e3c90cae47d12da21b41002b778
Carlos Zoido <mrgalleta@gmail.com>
2026-06-11 17:44:01 +0200

    try to fix free-threaded

--- conanfile.py
+++ conanfile.py
@@ -1,11 +1,13 @@
+import sys
+import sysconfig
+
 from conan import ConanFile
-from conan.tools.cmake import CMake, cmake_layout
+from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
 
 
 class PycapnpConan(ConanFile):
     name = "pycapnp"
     settings = "os", "compiler", "build_type", "arch"
-    generators = "CMakeToolchain", "CMakeDeps"
 
     def requirements(self):
         self.requires("capnproto/1.3.0")
@@ -13,11 +15,16 @@ class PycapnpConan(ConanFile):
     def layout(self):
         cmake_layout(self)
 
-    def build(self):
-        import sys
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.cache_variables["Python3_EXECUTABLE"] = sys.executable
+        tc.cache_variables["Python3_INCLUDE_DIR"] = sysconfig.get_path("include")
+        tc.generate()
+        CMakeDeps(self).generate()
 
+    def build(self):
         cmake = CMake(self)
-        cmake.configure(variables={"Python3_EXECUTABLE": sys.executable})
+        cmake.configure()
         cmake.build()
 
     def package(self):
