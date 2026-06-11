6c53c1a1716b4713b7fab449de62fddd212e6747
Carlos Zoido <mrgalleta@gmail.com>
2026-06-11 14:03:14 +0200

    migrate build system from setuptools to conan-py-build

---
+++ conan-build.profile
@@ -0,0 +1,5 @@
+[platform_tool_requires]
+m4/1.4.19
+autoconf/2.71
+automake/1.16.5
+libtool/2.4.7
---
+++ conanfile.py
@@ -0,0 +1,23 @@
+from conan import ConanFile
+from conan.tools.cmake import CMake, cmake_layout
+
+
+class PycapnpConan(ConanFile):
+    name = "pycapnp"
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "CMakeToolchain", "CMakeDeps"
+
+    def requirements(self):
+        self.requires("capnproto/1.3.0")
+
+    def layout(self):
+        cmake_layout(self)
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def package(self):
+        cmake = CMake(self)
+        cmake.install()
